b316746ac1a1d2d43d0909a42c159e1dc988ce8d
jwynveen <jonwynveen@gmail.com>
2012-05-03 21:10:02 -0500

    Use responsive boilerplate

--- index.php
+++ index.php
@@ -1,79 +1,98 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head lang="en" xml:lang="en">
-	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-	<title>Test page</title>
-	<style type="text/css">
-		body {
-			font-family: Arial, Helvetica, sans-serif;
-			font-size: .9em;
-			color: #000000;
-			background-color: #FFFFFF;
-			margin: 0;
-			padding: 10px 20px 20px 20px;
-		}
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
-		samp {
-			font-size: 1.3em;
-		}
+	<title></title>
+	<meta name="description" content="">
+	<meta name="author" content="">
 
-		a {
-			color: #000000;
-			background-color: #FFFFFF;
-		}
-		
-		sup a {
-			text-decoration: none;
-		}
+	<meta name="viewport" content="width=device-width">
 
-		hr {
-			margin-left: 90px;
-		    height: 1px;
-		    color: #000000;
-			background-color: #000000;
-		    border: none;
-		}
+	<link rel="stylesheet" href="css/style.css">
 
-		#logo {
-			margin-bottom: 10px;
-		}
-
-		.text {
-			width: 80%;
-			margin-left: 90px;
-			line-height: 140%;
-		}
-	</style>
+	<script src="js/libs/modernizr-2.5.3-respond-1.1.0.min.js"></script>
 </head>
-
 <body>
 	<div id="fb-root"></div>
-<script>
-  window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '380172018700591', // App ID
-      channelUrl : '//localhost:8888/Mustached/channel.html', // Channel File
-      status     : true, // check login status
-      cookie     : true, // enable cookies to allow the server to access the session
-      xfbml      : true  // parse XFBML
-    });
+	<script>
+	  window.fbAsyncInit = function() {
+	    FB.init({
+	      appId      : '380172018700591', // App ID
+	      channelUrl : '//localhost:8888/Mustached/channel.html', // Channel File
+	      status     : true, // check login status
+	      cookie     : true, // enable cookies to allow the server to access the session
+	      xfbml      : true  // parse XFBML
+	    });
+	
+	    // Additional initialization code here
+	    FB.Canvas.setAutoGrow();
+	    FB.login(function(response) {
+	      // handle the response
+	    }, {scope: 'email,user_likes'});
+	  };
+	
+	  // Load the SDK Asynchronously
+	  (function(d){
+	     var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+	     if (d.getElementById(id)) {return;}
+	     js = d.createElement('script'); js.id = id; js.async = true;
+	     js.src = "//connect.facebook.net/en_US/all.js";
+	     ref.parentNode.insertBefore(js, ref);
+	   }(document));
+	</script>
+	<div id="header-container">
+		<header class="wrapper clearfix">
+			<h1 id="title">h1#title</h1>
+			<nav>
+				<ul>
+					<li><a href="#">nav ul li a</a></li>
+					<li><a href="#">nav ul li a</a></li>
+					<li><a href="#">nav ul li a</a></li>
+				</ul>
+			</nav>
+		</header>
+	</div>
+	<div id="main-container">
+		<div id="main" class="wrapper clearfix">
+			
+			<article>
+				<header>
+					<h1>article header h1</h1>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec.</p>
+				</header>
+				<section>
+					<h2>article section h2</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec. Curabitur posuere enim eget turpis feugiat tempor. Etiam ullamcorper lorem dapibus velit suscipit ultrices. Proin in est sed erat facilisis pharetra.</p>
+				</section>
+				<section>
+					<h2>article section h2</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec. Curabitur posuere enim eget turpis feugiat tempor. Etiam ullamcorper lorem dapibus velit suscipit ultrices. Proin in est sed erat facilisis pharetra.</p>
+				</section>
+				<footer>
+					<h3>article footer h3</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec. Curabitur posuere enim eget turpis feugiat tempor.</p>
+				</footer>
+			</article>
+			
+			<aside>
+				<h3>aside</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec. Curabitur posuere enim eget turpis feugiat tempor. Etiam ullamcorper lorem dapibus velit suscipit ultrices.</p>
+			</aside>
+			
+		</div> <!-- #main -->
+	</div> <!-- #main-container -->
 
-    // Additional initialization code here
-    FB.login(function(response) {
-      // handle the response
-    }, {scope: 'email,user_likes'});
-  };
+	<div id="footer-container">
+		<footer class="wrapper">
+			<h3>footer</h3>
+		</footer>
+	</div>
 
-  // Load the SDK Asynchronously
-  (function(d){
-     var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement('script'); js.id = id; js.async = true;
-     js.src = "//connect.facebook.net/en_US/all.js";
-     ref.parentNode.insertBefore(js, ref);
-   }(document));
-</script>
-	Test page...
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/libs/jquery-1.7.2.min.js"><\/script>')</script>
+
+<script src="js/script.js"></script>
 </body>
 </html>
